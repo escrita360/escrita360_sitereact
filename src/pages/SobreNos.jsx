dcd67321be7b4304a538a06ecc31a63d90861c2f
VictorMartins086 <martinsvictor086@gmail.com>
2025-11-28 10:59:55 -0400

    imagens do carrossel alterada

--- src/pages/SobreNos.jsx
+++ src/pages/SobreNos.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
-import port1 from '../assets/Portfolio/port1.jpg';
-import port4 from '../assets/Portfolio/port4.jpg';
-import port5 from '../assets/Portfolio/port5.jpg';
-import port7 from '../assets/Portfolio/port7.jpg';
-import port8 from '../assets/Portfolio/port8.jpg';
-import port9 from '../assets/Portfolio/port9.jpg';
-import port10 from '../assets/Portfolio/port10.jpg';
-import port11 from '../assets/Portfolio/port11.jpg';
+import img1 from '../assets/Portfolio/1.svg';
+import img3 from '../assets/Portfolio/3.svg';
+import img4 from '../assets/Portfolio/4.svg';
+import img5 from '../assets/Portfolio/5.svg';
+import img6 from '../assets/Portfolio/6.svg';
+import img7 from '../assets/Portfolio/7.svg';
+import img8 from '../assets/Portfolio/8.svg';
+import img9 from '../assets/Portfolio/9.svg';
+import img10 from '../assets/Portfolio/10.svg';
+import img11 from '../assets/Portfolio/11.svg';
+import img12 from '../assets/Portfolio/12.svg';
+import img13 from '../assets/Portfolio/13.svg';
+import img14 from '../assets/Portfolio/14.svg';
+import img15 from '../assets/Portfolio/15.svg';
+import img16 from '../assets/Portfolio/16.svg';
+import img17 from '../assets/Portfolio/17.svg';
+import img18 from '../assets/Portfolio/18.svg';
+import img19 from '../assets/Portfolio/19.svg';
+import img20 from '../assets/Portfolio/20.svg';
+import img21 from '../assets/Portfolio/21.svg';
+import img22 from '../assets/Portfolio/22.svg';
+import img23 from '../assets/Portfolio/23.svg';
 
 export default function SobreNos() {
-  const images = [port1, port4, port5, port7, port8, port9, port10, port11];
+  const images = [img1, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const next = () => setCurrentIndex((prev) => (prev + 1) % images.length);
