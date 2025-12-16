4f056aa8dfab98aba20e2cacef336e19f48e920a
VictorMartins086 <martinsvictor086@gmail.com>
2025-12-16 12:33:20 -0400

    feat: Update monthly and yearly prices for 'Plano Mensal'

--- src/pages/Planos.jsx
+++ src/pages/Planos.jsx
@@ -38,8 +38,8 @@ function Precos() {
     {
       name: 'Plano Mensal',
       badge: 'Preço promocional de lançamento',
-      monthlyPrice: 49,
-      yearlyPrice: 588,
+      monthlyPrice: 120,
+      yearlyPrice: 1440,
       description: 'Projetado para uso individual e motivacional',
       subDescription: 'Aproveite o preço promocional de lançamento da Plataforma Escrita360',
       credits: 50,
